e251c6a05c75061d7bff89109db14f4b6bb0e8e4
anasa <anas-aji-seeroo>
2023-09-07 18:12:17 +0100

    Deployment

--- blog-frontend/src/utils/network_utils.js
+++ blog-frontend/src/utils/network_utils.js
@@ -1,5 +1,5 @@
 // export const baseUrl = "http://localhost:4000";
-export const baseUrl = "https://elated-bat-pants.cyclic.cloud";
+export const baseUrl = "https://blog-app-mern-952d.onrender.com";
 
 export const fetchRequestOption = (reqMethod, data)  => {
     return {
